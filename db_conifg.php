27e736fde3c1b535aa7c2eb29a29ea3051a0611f
erdrindemi🐉 <erdrindemi2008@gmail.com>
2025-01-23 17:45:33 +0100

    ..

--- db_conifg.php
+++ db_conifg.php
@@ -1,17 +1,11 @@
 <?php
 $host = 'localhost';      
-$db = 'game_preregistration';  
+$db = 'personalproject';  
 $user = 'root';           
 $password = '';          
 
-
-$conn = new mysql($host, $user, $password, $db);
-
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-?>
-
-
-
+try{
+    $conn = new PDO("mysql:host=$host;dbname=$db",$user,$password);
+}catch (PDOExpection $e) {
+    echo "error: " . $e->getMessage();
+} 
